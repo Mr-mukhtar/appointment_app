5a9de7f5cb9dc00e1965f96164080133f3b8082c
mr-mukhtar <mukhtarmohd517@gmail.com>
2023-07-19 10:08:11 +0530

    delete btn

--- demo.js
+++ demo.js
@@ -1,18 +1,29 @@
 var form = document.getElementById('my-form');
 var itemList = document.getElementById('items');
+// form submit
 form.addEventListener('submit', onsubmit);
+// delete event
+itemList.addEventListener('click', deleteItem)
 function onsubmit(e){
     e.preventDefault();
      // get the value
     var newName = document.getElementById('name').value;
     var newEmail = document.getElementById('email').value;
     var newNum = document.getElementById('number').value;
+    //create li
      var li = document.createElement('li');
      li.className= 'list-group';
      // Add text node with input value
-     li.appendChild(document.createTextNode("name: "+newName));
-     li.appendChild(document.createTextNode(" Email: "+newEmail));
-     li.appendChild(document.createTextNode(" number: "+newNum));
+     li.appendChild(document.createTextNode(newName));
+     li.appendChild(document.createTextNode(" ="+newEmail));
+     li.appendChild(document.createTextNode(" ="+newNum));
+
+     //create a button
+     var deleteBtn = document.createElement('button');
+     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+     deleteBtn.appendChild(document.createTextNode('delete'));
+     li.appendChild(deleteBtn)
      itemList.appendChild(li);
 
      var userDetails = {
@@ -21,4 +32,12 @@ function onsubmit(e){
         newNum:newNum,
      }
      localStorage.setItem("userDetails", JSON.stringify(userDetails));
+}
+function deleteItem(e){
+   if(e.target.classList.contains('delete')){
+      if(confirm('Are you Sure?')){
+         var li =e.target.parentElement;
+         itemList.removeChild(li);
+      }
+   }
 }
